7d7569373a59e544e0064f06a5a6116008686742
Ahmad Al Msouty <ahmad.msouti@Hotmail.com>
2017-07-06 13:54:39 +0300

    h

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module ApiTest
     end
 
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 5.1
+    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
@@ -33,6 +33,6 @@ module ApiTest
     # Don't generate system test files.
     # config.assets.initialize_on_precompile = false
 
-    # config.generators.system_tests = nil
+    config.generators.system_tests = nil
   end
 end
