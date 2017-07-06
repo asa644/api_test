85ff83f33e20c75f2f636c58ada14ce6104b6cef
Ahmad Al Msouty <ahmad.msouti@Hotmail.com>
2017-07-06 16:03:02 +0300

    deleted assets heroku

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,8 @@ module ApiTest
     # -- all .rb files in that directory are automatically loaded.
 
     # Don't generate system test files.
-    config.assets.initialize_on_precompile = false
+# config.assets.enabled = true
+    # config.assets.initialize_on_precompile = false
 
     config.generators.system_tests = nil
   end
